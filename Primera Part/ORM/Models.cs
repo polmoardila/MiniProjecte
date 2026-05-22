907d988c561bdc957f5831a1624f0df4b9639413
polmoardila <polmoardila@cendrassos.net>
2026-05-22 18:28:23 +0200

    feat: Add Docker support for MVC and ORM, include connection strings, and new CSV files

--- Primera Part/ORM/Models.cs
+++ Primera Part/ORM/Models.cs
@@ -1,11 +1,19 @@
 using CsvHelper.Configuration.Attributes;
-using MiniProjecte.Models;
 
 public class LiniaCsv
 {
-    [Name("Dia")] public string Dia { get; set; }
-    [Name("Estació")] public string Estacio { get; set; }
-    [Name("Nivell absolut (msnm)")] public string Nivell { get; set; }
-    [Name("Percentatge volum embassat (%)")] public string Percentatge { get; set; }
-    [Name("Volum embassat (hm3)")] public string Volum { get; set; }
+    [Name("Dia")] 
+    public string Dia { get; set; } = "";
+
+    [Name("Estació")] 
+    public string Estacio { get; set; } = "";
+
+    [Name("Nivell absolut (msnm)")] 
+    public string Nivell { get; set; } = "";
+
+    [Name("Percentatge volum embassat (%)")] 
+    public string Percentatge { get; set; } = "";
+
+    [Name("Volum embassat (hm3)")] 
+    public string Volum { get; set; } = "";
 }
